3b9a3f1f4946170c2113b0cfaf2cdd2952022a18
skycloudd <skycloudd@users.noreply.github.com>
2024-08-12 16:33:04 +0000

    deploy: 5054fada9a7c34ff6ba375bc465c6b7d278001dc

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"exolvl":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"exolvl/error/enum.Error.html\" title=\"enum exolvl::error::Error\">Error</a>"]]
+"exolvl":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"exolvl/error/enum.Error.html\" title=\"enum exolvl::error::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
